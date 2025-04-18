066b38aea03b6d532fbc035d6ea4d68902c28882
suhani5267 <suhanikaushik2003@gmail.com>
2025-04-18 22:46:04 +0530

    add some funcnality

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-// script.js
+
 
 let gameSeq = [];
 let userSeq = [];
-
+let ClickSound = new Audio("/sound/sound.mp3");
+let wrongSound = new Audio("/sound/wrongSound.mp3");
+let startSound = new Audio("/sound/game-start.mp3");
+let flashDuration = 250;
 let colors = ["yellow", "red", "green", "blue"];
 let level = 0;
+let highScore = localStorage.getItem("highScore") || 0;
+
 let started = false;
 
 let h2 = document.querySelector("h2");
@@ -15,6 +20,7 @@ console.log(startBtn)
 startBtn.addEventListener("click", () => {
     if (started == false) {
         started = true;
+        StartSound();
         levelUp();
     }
 })
@@ -29,22 +35,41 @@ function levelUp() {
     
     gameSeq.push(randColor);
     console.log(gameSeq)
+    let difficulty = document.querySelector("#difficulty").value;
+    switch (difficulty) {
+        case "easy":
+            flashDuration = 400;
+            break;
+        case "medium":
+            flashDuration = 250;
+            break;
+        case "hard":
+            flashDuration = 100;
+            break;
+    
+        default:
+            flashDuration = 250;
+            break;
+    }
 
   gameFlash(randBtn);
 }
 
 function gameFlash(btn) {
-  btn.classList.add("flash");
+    btn.classList.add("flash");
+    let btnId = btn.getAttribute("id");
+   
   setTimeout(() => {
     btn.classList.remove("flash");
-  }, 250);
+  }, flashDuration);
 }
 
 function userFlash(btn) {
-  btn.classList.add("userflash");
+    btn.classList.add("userflash");
+     PlaySound()
   setTimeout(() => {
     btn.classList.remove("userflash");
-  }, 250);
+  }, flashDuration);
 }
 
 function checkAns(index) {
@@ -53,9 +78,15 @@ function checkAns(index) {
       setTimeout(levelUp, 1000);
     }
   } else {
-    h2.innerHTML = `Game Over! Your score was <br>${level} <br>Press Any Key to Restart.`;
+      playWrong()
+      if (level > highScore) {
+          highScore = level;
+          localStorage.setItem("highScore",highScore)
+      }
+       
+    h2.innerHTML = `Game Over! Your score was <br>${level} <br> <br>High Score :${highScore}</br>Press Any Key to Restart.`;
    
-   
+  
     resetGame();
   }
 }
@@ -66,6 +97,7 @@ function btnClick() {
 
     let color = btn.getAttribute("id");
     userFlash(btn);
+    PlaySound(color)
     userSeq.push(color);
     console.log(userSeq)
 
@@ -78,6 +110,20 @@ console.log(allBtns)
 for (let btn of allBtns) {
     btn.addEventListener("click", btnClick);
     console.log(btn)
+}
+
+function PlaySound() {
+    ClickSound.currentTime = 0;
+    ClickSound.play();
+}
+function playWrong() {
+    wrongSound.currentTime = 0;
+    wrongSound.play();
+}
+
+function StartSound() {
+    startSound.currentTime = 0;
+    startSound.play();
 }
 
 function resetGame() {
